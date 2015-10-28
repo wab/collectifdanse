ab117f381efdb9684e6a14f7884e79d0ab295881
wab <wab2com@gmail.com>
2015-10-28 17:08:58 +0100

    add event thumb image size

--- web/app/themes/cdrm/lib/setup.php
+++ web/app/themes/cdrm/lib/setup.php
@@ -35,6 +35,7 @@ function setup() {
   // http://codex.wordpress.org/Function_Reference/set_post_thumbnail_size
   // http://codex.wordpress.org/Function_Reference/add_image_size
   add_theme_support('post-thumbnails');
+  add_image_size( 'event', 400, 300, true );
 
   // Enable post formats
   // http://codex.wordpress.org/Post_Formats
