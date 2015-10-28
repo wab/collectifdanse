a1f688d1c82406c44df6ea4c19298d124966701b
wab <wab2com@gmail.com>
2015-10-28 17:10:25 +0100

    site-title font size

--- web/app/themes/cdrm/templates/header.php
+++ web/app/themes/cdrm/templates/header.php
@@ -1,7 +1,7 @@
 <header class="mainheader" role="banner">
   <div class="container">
   	<div class="header-wrapper">
-	    <h1><span class="text-info">Collectif<br>Danse</span><br>  <span class="text-primary">Rennes<br> Métropole</span></h1>
+	    <h1 class="site-title"><span class="text-info">Collectif<br>Danse</span><br>  <span class="text-primary">Rennes<br> Métropole</span></h1>
 	    <button class="menu-button" id="open-button"><span class="fa fa-bars"></span><br>menu</button>
     </div>
   </div>
