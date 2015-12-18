e081d5198358540be5ee94168c0981aadee79a84
wab <contact@wabdesign.fr>
2015-12-18 15:27:37 +0100

    back carousel header

--- site/web/app/themes/cdrm/templates/header.php
+++ site/web/app/themes/cdrm/templates/header.php
@@ -1,8 +1,14 @@
 <header class="mainheader" role="banner">
-  <div class="container">
-  	<div class="header-wrapper">
-	    <h1 class="site-title"><span class="text-info">Collectif<br>Danse</span><br>  <span class="text-primary">Rennes<br> Métropole</span></h1>
-    </div>
-  </div>
+	<div class="container">
+		<div class="header-wrapper">
+	    	<h1 class="site-title"><span class="text-info">Collectif<br>Danse</span><br>  <span class="text-primary">Rennes<br> Métropole</span></h1>
+		</div>
+	</div>
 	<button class="menu-button" id="open-button"><span class="fa fa-bars"></span><br>menu</button>
+	<ul class="cb-slideshow list-unstyled">
+		<li><span>image 1</span></li>
+		<li><span>image 2</span></li>
+		<li><span>image 3</span></li>
+		<li><span>image 4</span></li>
+	</ul>
 </header>
